e12315b783ad5cbc1787ac226694ff0856f48585
Sora <34794115+sorae42@users.noreply.github.com>
2024-11-13 15:49:09 +0700

    feat: working auth (wip)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,23 @@
 	import { Search, History, List, UserRound, Bookmark } from 'lucide-svelte';
 	import { TabGroup, TabAnchor } from '@skeletonlabs/skeleton';
 	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
+	import { invalidate } from '$app/navigation';
+
+	export let data;
+
+	let { supabase, session } = data;
+	$: ({ supabase, session } = data);
+
+	onMount(() => {
+		const { data } = supabase.auth.onAuthStateChange((event, newSession) => {
+			if (newSession?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth');
+			}
+		});
+
+		return () => data.subscription.unsubscribe();
+	});
 
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 </script>
@@ -20,8 +37,12 @@
 				<TabAnchor href="/" selected={$page.url.pathname === '/'}>Phim mới</TabAnchor>
 				<TabAnchor href="/phim-le" selected={$page.url.pathname === '/phim-le'}>Phim lẻ</TabAnchor>
 				<TabAnchor href="/phim-bo" selected={$page.url.pathname === '/phim-bo'}>Phim bộ</TabAnchor>
-				<TabAnchor href="/tv-shows" selected={$page.url.pathname === '/tv-shows'}>TV Shows</TabAnchor>
-				<TabAnchor href="/hoat-hinh" selected={$page.url.pathname === '/hoat-hinh'}>Hoạt hình</TabAnchor>
+				<TabAnchor href="/tv-shows" selected={$page.url.pathname === '/tv-shows'}
+					>TV Shows</TabAnchor
+				>
+				<TabAnchor href="/hoat-hinh" selected={$page.url.pathname === '/hoat-hinh'}
+					>Hoạt hình</TabAnchor
+				>
 			</TabGroup>
 			<svelte:fragment slot="trail">
 				<a class="btn btn-sm variant-ghost-surface" href="/" target="_blank" rel="noreferrer">
@@ -36,7 +57,7 @@
 				<a class="btn btn-sm variant-ghost-surface" href="/" target="_blank" rel="noreferrer">
 					<History />
 				</a>
-				<a class="btn btn-sm variant-ghost-surface" href="/" target="_blank" rel="noreferrer">
+				<a class="btn btn-sm variant-ghost-surface" href="/auth">
 					<UserRound />
 				</a>
 			</svelte:fragment>
