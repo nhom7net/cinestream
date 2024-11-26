70e49d56932e5a9881227a700feecb4f3cb5f8cc
Jin <nhoanghai2003@gmail.com>
2024-11-26 10:04:29 +0700

    hoàn thành trang chi tiết phim

--- src/components/MovieCard.svelte
+++ src/components/MovieCard.svelte
@@ -7,10 +7,10 @@
 	}
 </script>
 
-<div
-	class="w-56 max-w-xs variant-filled-surface rounded-lg overflow-hidden text-center shadow-md transition-transform duration-200 ease-in-out transform hover:scale-105 hover:variant-soft-tertiary"
->
-	<img src={url + data.poster_url} alt={data.name} class="w-full h-72 object-cover" />
-	<p class="text-sm font-semibold text-orange-400 my-2 text-wrap">{data.name}</p>
-	<p class="text-sm text-gray-300 mb-3 text-wrap">{data.origin_name}</p>
-</div>
+<a href="/details/{data.slug}">
+	<div class="w-56 max-w-xs variant-filled-surface rounded-lg overflow-hidden text-center shadow-md transition-transform duration-200 ease-in-out transform hover:scale-105 hover:variant-soft-tertiary">
+		<img src={url + data.poster_url} alt={data.name} class="w-full h-72 object-cover" />
+		<p class="text-sm font-semibold text-orange-400 my-2 text-wrap">{data.name}</p>
+		<p class="text-sm text-gray-300 mb-3 text-wrap">{data.origin_name}</p>
+	</div>
+</a>
--- src/routes/[genreSlug]/+page.ts
+++ src/routes/[genreSlug]/+page.ts
@@ -2,7 +2,7 @@ import type { PageLoad } from "./$types";
 
 export const load: PageLoad = ({ params }) => {
     const allowedSelections: string[] = ["phim-moi-cap-nhat", "hoat-hinh", "phim-le", "phim-bo", "tv-shows"];
-    let url: string = `https://phimapi.com/v1/api/danh-sach/${params.genreSlug}?page=`;
+    const url: string = `https://phimapi.com/v1/api/danh-sach/${params.genreSlug}?page=`;
     
     if (!allowedSelections.includes(params.genreSlug)) throw new Error("Not Found");
     
