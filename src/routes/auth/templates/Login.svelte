9eb8f875fbbcbb7ac12d29b8342bbd2b38695da4
Sora <34794115+sorae42@users.noreply.github.com>
2024-11-30 01:29:35 +0700

    feat(auth): real time validation

--- src/routes/auth/templates/Login.svelte
+++ src/routes/auth/templates/Login.svelte
@@ -9,7 +9,7 @@
 
 	const handleSubmit: SubmitFunction = () => {
 		loading = true;
-		
+
 		return async ({ update }) => {
 			update();
 			loading = false;
@@ -20,47 +20,46 @@
 <h2 class="h2">Đăng nhập</h2>
 <div class="py-2" />
 
-<div class="flex flex-col">
-	<form class="flex flex-center w-full" action="?/login" method="POST" use:enhance={handleSubmit}>
-		<div class="col-6 form-widget">
-			<div>
-				<label for="email">
-					<span>Địa chỉ email</span>
-				</label>
-				<div class="s-input-group">
-					<div class="input-group-shim"><AtSign /></div>
-					<input
-						id="email"
-						name="email"
-						class="input"
-						type="email"
-						placeholder="user@example.com"
-						value={form?.email ?? ''}
-						required
-					/>
-				</div>
-			</div>
-			<div>
-				<label for="password">
-					<span>Mật khẩu</span>
-				</label>
-				<div class="s-input-group">
-					<div class="input-group-shim"><LockKeyhole /></div>
-					<input
-						id="password"
-						name="password"
-						class="input"
-						type="password"
-						placeholder="••••••••"
-						required
-					/>
-				</div>
-			</div>
-			<div class="my-4">
-				<button class="variant-filled-primary w-full">
-					{loading ? 'Loading' : 'Đăng nhập'}
-				</button>
-			</div>
+<form
+	class="flex flex-col flex-center w-full [&>*]:my-1"
+	action="?/login"
+	method="POST"
+	use:enhance={handleSubmit}
+>
+	<div>
+		<label for="email">
+			<span>Địa chỉ email</span>
+		</label>
+		<div class="s-input-group">
+			<div class="input-group-shim"><AtSign /></div>
+			<input
+				id="email"
+				name="email"
+				class="input"
+				type="email"
+				placeholder="user@example.com"
+				value={form?.email ?? ''}
+				required
+			/>
 		</div>
-	</form>
-</div>
+	</div>
+	<div>
+		<label for="password">
+			<span>Mật khẩu</span>
+		</label>
+		<div class="s-input-group">
+			<div class="input-group-shim"><LockKeyhole /></div>
+			<input
+				id="password"
+				name="password"
+				class="input"
+				type="password"
+				placeholder="••••••••"
+				required
+			/>
+		</div>
+	</div>
+	<div class="my-4">
+		<button class="variant-filled-primary w-full my-2">Đăng nhập</button>
+	</div>
+</form>
