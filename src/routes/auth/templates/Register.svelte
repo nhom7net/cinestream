b0f38586d06ceb33d381b0982a7d9d67dc0ea49e
Sora <34794115+sorae42@users.noreply.github.com>
2024-11-30 01:29:35 +0700

    feat(auth): real time validation

--- src/routes/auth/templates/Register.svelte
+++ src/routes/auth/templates/Register.svelte
@@ -6,30 +6,61 @@
 	export let form: ActionData;
 	let loading = false;
 
-    const handleSubmit: SubmitFunction = () => {
+	const handleSubmit: SubmitFunction = () => {
 		loading = true;
 		return async ({ update }) => {
 			update();
 			loading = false;
 		};
 	};
+
+	// sanity checking
+	let password: string;
+	let passconf: string;
+	let passwordError: string = '';
+	let passconfError: string = '';
+
+	function validation() {
+		if (password.length === 0) {
+			passwordError = '';
+			passconfError = '';
+			loading = false;
+			return;
+		}
+
+		if (password.length <= 8) {
+			passwordError = 'Mật khẩu phải 8 kí tự trở lên!';
+			loading = true;
+		} else passwordError = '';
+
+		if (passconf.length <= 8 || password !== passconf) {
+			passconfError = 'Mật khẩu không trùng khớp!';
+			loading = true;
+		} else {
+			passconfError = '';
+			loading = false;
+		}
+	}
 </script>
 
 <h2 class="h2">Đăng ký</h2>
 <div class="py-2" />
 
 <div class="flex flex-col">
-	<form class="flex flex-col flex-center" action="?/register" method="post" use:enhance={handleSubmit}>
-		<div class="col-6 form-widget">
-			<label for="name">
-				<span>Tên hiển thị</span>
-			</label>
+	<form
+		class="flex flex-col flex-center [&>*]:my-1"
+		action="?/register"
+		method="post"
+		use:enhance={handleSubmit}
+	>
+		<div>
+			<label for="name">Tên hiển thị</label>
 			<div class="s-input-group">
 				<div class="input-group-shim"><User /></div>
 				<input id="name" name="name" class="input" type="text" placeholder="Van An" required />
 			</div>
 		</div>
-		<div class="col-6 form-widget">
+		<div>
 			<label for="username">
 				<span>Tên đăng nhập</span>
 			</label>
@@ -41,15 +72,13 @@
 					class="input"
 					type="text"
 					placeholder="user"
-					value={form?.username?? ''}
+					value={form?.username ?? ''}
 					required
 				/>
 			</div>
 		</div>
-		<div class="col-6 form-widget">
-			<label for="email">
-				<span>Địa chỉ email</span>
-			</label>
+		<div>
+			<label for="email">Địa chỉ email</label>
 			<div class="s-input-group">
 				<div class="input-group-shim"><AtSign /></div>
 				<input
@@ -63,10 +92,8 @@
 				/>
 			</div>
 		</div>
-		<div class="col-6 form-widget">
-			<label for="password">
-				<span>Mật khẩu</span>
-			</label>
+		<div>
+			<label for="password">Mật khẩu</label>
 			<div class="s-input-group">
 				<div class="input-group-shim"><LockKeyhole /></div>
 				<input
@@ -76,13 +103,14 @@
 					type="password"
 					placeholder="••••••••"
 					required
+					bind:value={password}
+					on:keyup={validation}
 				/>
 			</div>
+			<p class="text-red-500">{passwordError}</p>
 		</div>
-		<div class="col-6 form-widget">
-			<label for="password-confirm">
-				<span>Xác nhận lại mật khẩu</span>
-			</label>
+		<div>
+			<label for="password-confirm">Xác nhận lại mật khẩu</label>
 			<div class="s-input-group">
 				<div class="input-group-shim"><LockKeyhole /></div>
 				<input
@@ -92,14 +120,15 @@
 					type="password"
 					placeholder="••••••••"
 					required
+					bind:value={passconf}
+					on:keyup={validation}
 				/>
 			</div>
+			<p class="text-red-500">{passconfError}</p>
 		</div>
 
 		<div class="my-4">
-			<button class="variant-filled-primary w-full">
-				{loading ? 'Loading' : 'Đăng ký'}
-			</button>
+			<button class="variant-filled-primary w-full" disabled={loading}>Đăng ký</button>
 		</div>
 	</form>
 </div>
