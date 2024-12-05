d4cfcb6ab6443b66912efc8edcb25158bbce7189
DredK18 <170698756+DredK18@users.noreply.github.com>
2024-12-05 21:26:52 +0700

    css lại trang book mark cho giống figma

--- src/routes/bookmark/+page.svelte
+++ src/routes/bookmark/+page.svelte
@@ -1,17 +1,30 @@
 <script lang="ts">
 	export let data;
 </script>
-<!-- Display the slug-movie array -->
-{#if data.slugMovies.length > 0}
-	<ul>
-		{#each data.slugMovies as movie}
-			<li>
-				<div>Name: {movie.name}</div>
-				<a href="/details/{movie.slug}" class="flex gap-2"><img src="{movie.poster_url}" alt="{movie.name}" width="100" /></a>
-				
-			</li>
-		{/each}
-	</ul>
-{:else}
-	<p>Loading movies...</p>
-{/if}
+
+<h1 class="text-4xl font-bold mt-6 pl-10">Phim đã yêu thích</h1>
+
+<div class="flex flex-col px-56 pt-20 mr">
+	{#if data.slugMovies.length > 0}
+		<ul class="grid grid-cols-2 gap-12">
+			{#each data.slugMovies as movie}
+				<li
+					class="flex items-center gap-4 p-4 border border-gray-200 rounded-lg shadow-sm hover:bg-gray-50"
+				>
+					<a href="/details/{movie.slug}" class="flex-shrink-0">
+						<img
+							src={movie.poster_url}
+							alt={movie.name}
+							class="w-20 h-30 object-cover rounded-md"
+						/>
+					</a>
+					<div class="flex-grow">
+						<div class="text-xl font-semibold text-white-800">{movie.name}</div>
+					</div>
+				</li>
+			{/each}
+		</ul>
+	{:else}
+		<p class="text-gray-500">Loading movies...</p>
+	{/if}
+</div>
