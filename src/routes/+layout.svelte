2645f5c729c27db039624f24f8397b572a0397c0
Sora <34794115+sorae42@users.noreply.github.com>
2024-12-15 02:20:37 +0700

    enhance(layout): drawer and link to admin landing page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,7 +8,13 @@
 		type PopupSettings,
 		popup,
 		ListBox,
-		ListBoxItem
+		ListBoxItem,
+
+		Drawer,
+
+		getDrawerStore
+
+
 	} from '@skeletonlabs/skeleton';
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
@@ -18,6 +24,7 @@
 	import { onMount } from 'svelte';
 	import { invalidate } from '$app/navigation';
 	import Avatar from '$lib/Avatar.svelte';
+	import UserInfo from '$lib/UserInfo.svelte';
 
 	initializeStores();
 
@@ -37,6 +44,8 @@
 	});
 
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+	const drawerStore = getDrawerStore();
+
 	let keyword: string = '';
 	let searchSuggestion: any[] = [];
 
@@ -82,6 +91,11 @@
 </script>
 
 <Toast position="bl" />
+<Drawer>
+	{#if $drawerStore.id === 'user-info'}
+		<UserInfo {supabase} userid={$drawerStore.meta.id}/>
+	{/if}
+</Drawer>
 
 <AppShell>
 	<svelte:fragment slot="header">
@@ -169,7 +183,10 @@
 	<!-- User button dialog popup -->
 	<div class="z-50 card p-4 variant-filled-surface" data-popup="userDialogPopup">
 		<ul class="list [&>*]:p-2 [&>*]:space-x-2 [&>*]:rounded">
-			<li class="hover:variant-ghost-primary"><a href="/profile">Trang cá nhân</a></li>
+			{#if data.admin}
+				<li class="hover:variant-ghost-primary"><a href="/admin">Admin quản lý</a></li>
+			{/if}
+			<li class="hover:variant-ghost-primary"><a href="/profile">Chỉnh sửa hồ sơ</a></li>
 			<li class="hover:variant-ghost-primary"><a href="/auth/signout">Đăng xuất</a></li>
 		</ul>
 	</div>
