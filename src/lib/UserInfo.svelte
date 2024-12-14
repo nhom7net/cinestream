ee44f8c9e1cbb829313220e860a1848365975065
Sora <34794115+sorae42@users.noreply.github.com>
2024-12-15 02:19:53 +0700

    feat(admin): trang quản lý người dùng (wip)

---
+++ src/lib/UserInfo.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type { SupabaseClient } from "@supabase/supabase-js";
+
+    export let supabase: SupabaseClient;
+    export let userid: string;
+</script>
+
+<div class="p-2">
+    <p>This panel is to manage user id {userid}</p>
+</div>
