879409ebdab47997d2becab9f7ccd2d62bdf8253
Jin <nhoanghai2003@gmail.com>
2024-12-27 22:16:39 +0700

    fix: i forgor 💀💀

--- src/routes/watch/[watchSlug]/+page.server.ts
+++ src/routes/watch/[watchSlug]/+page.server.ts
@@ -3,7 +3,7 @@ import type { PageServerLoad } from "../$types";
 export const load: PageServerLoad = async ({ params, locals: { supabase } }) => {
     const { data: comment, error } = await supabase
         .from('comments')
-        .select('id, user_id, movie_id, comment, created_at, profiles(full_name, avatar_url)')
+        .select('id, user_id, movie_id, comment, created_at, ...profiles(full_name, avatar_url)')
         .eq('movie_id', params.watchSlug)
 
     if (error) {
